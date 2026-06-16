bfa71000622457117b26be0bf44424a81a7aa5bd
Elie12-d <elienomenjanahary35@gmail.com>
2026-06-16 15:41:15 +0300

    creation du controllerako

---
+++ framework/src/main/java/annotation/Controllerako.java
@@ -0,0 +1,14 @@
+package annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) // ndique a Java de conserver l'annotation dans le fichier .class 
+// et la rendre accessible via la réflexion lors de l'execution.
+
+@Target(ElementType.TYPE) // .TYPE pour les class
+public @interface Controllerako {
+    
+}
