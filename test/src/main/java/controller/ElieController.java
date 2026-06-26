7b3fb400a66c17f64629f4d7cdedbfcb1b33819b
Elie12-d <elienomenjanahary35@gmail.com>
2026-06-26 03:47:50 +0300

    valider

--- test/src/main/java/controller/ElieController.java
+++ test/src/main/java/controller/ElieController.java
@@ -1,8 +1,19 @@
 package controller;
 
 import annotation.Controllerako;
+import annotation.GetMapping;
 
 @Controllerako
 public class ElieController {
-    String nom;
+    @GetMapping("/hello")
+    public void hello() {
+    }
+
+    @GetMapping("/test")
+    public void test() {
+    }
+
+    @GetMapping // URL vide
+    public void index() {
+    }
 }
